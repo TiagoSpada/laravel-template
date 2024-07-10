c8b25885795e803595474c305576a9b0947e2c3a
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-10 16:51:11 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Template </title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    
+    <h1 class="text-primary my-4">Template Laravel</h1>
+
+    <img src="{{ Vite::asset('resources/img/loki.jpeg') }}" alt="loki">
+
+    
+</body>
+</html>
